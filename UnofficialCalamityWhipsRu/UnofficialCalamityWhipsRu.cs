0427d391b766d156409e363e202b13a9b2e93add
Matik-501 <klimahinmatvej3@gmail.com>
2025-04-06 21:35:55 +0400

    Add files via upload

---
+++ UnofficialCalamityWhipsRu/UnofficialCalamityWhipsRu.cs
@@ -0,0 +1,25 @@
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria;
+using System.Reflection;
+
+namespace UnofficialCalamityWhipsRu
+{
+	public class UnofficialCalamityWhipsRu : Mod
+	{
+		public static Mod UnofficialCalamityWhips;
+
+
+        public static string buffPath = "UnofficialCalamityWhips/DefaultDebuff";
+
+		public override void Load() {
+			if (ModLoader.HasMod("UnofficialCalamityWhips")) {
+                UnofficialCalamityWhips = ModLoader.GetMod("UnofficialCalamityWhips");
+            }
+			else {
+				UnofficialCalamityWhips = null;
+            }
+		}
+		
+	}
+}
